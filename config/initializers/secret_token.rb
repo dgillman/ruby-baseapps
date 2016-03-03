137f7f7dd86e77bd45fedfb365642d90284514e3
duffy@rsmart.com <duffy.gillman@gmail.com>
2016-03-03 15:08:36 -0700

    rails-3.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '2b361a31555f052371660cbea13afedeb755f66cdf9362bb505b8353057887a092217f6d17c6952e1dedccf05a0afaaf906faaac38ecdf96c1958ee789573c28'
