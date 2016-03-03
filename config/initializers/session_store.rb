9b697c7bdc2a221d220c663ff97c62bba3e11e17
duffy@rsmart.com <duffy.gillman@gmail.com>
2016-03-03 16:15:38 -0700

    4.1.14.2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Helloworld::Application.config.session_store :cookie_store, key: '_helloworld_session'
+Rails.application.config.session_store :cookie_store, key: '_helloworld_session'
