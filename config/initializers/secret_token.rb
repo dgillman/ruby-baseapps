d8b134069026e6ee06b65d9d1bcf4bde1e6451b0
duffy@rsmart.com <duffy.gillman@gmail.com>
2016-03-03 16:04:20 -0700

    rails-4.0.13

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Helloworld::Application.config.secret_token = '4add6ffcb21fcb176e526bf98b3e957a22d663178fcb0bc0263c73fa572bd3596053f232586965d2a2d357ae9c082103848e8273319ea0b6148bf52083d73bd2'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Helloworld::Application.config.secret_key_base = 'dae7c7be0aece9347ebf56e806d9e17a5234b6e978e54d26b36717b23751e5a6efb1eec49bba1ead469ed141d49b92b6741f6f08c8b381388f7d95210d2ecba0'
