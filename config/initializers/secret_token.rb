74c9b179f3c3ed03bb4d939a21eb5c06e45ecec5
duffy@rsmart.com <duffy.gillman@gmail.com>
2016-03-03 15:51:41 -0700

    rails-3.2.22.2

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Helloworld::Application.config.secret_token = 'a37c7c0467cd63a47c3c04bb2464edf166a1803eda0285754270eba815cf515c0facb814fb9df932cc52ae2b4d57894414f167988bd818a0658a7117d9915bc8'
+Helloworld::Application.config.secret_token = '4add6ffcb21fcb176e526bf98b3e957a22d663178fcb0bc0263c73fa572bd3596053f232586965d2a2d357ae9c082103848e8273319ea0b6148bf52083d73bd2'
